2c8e60285d0fefdcfe3a8d6ec9efc172c85b8e1a
Brook06 <durantebrook@gmail.com>
2023-12-23 17:26:53 +0100

    Prima definizione della classe Robot  e della funzione separation ( Distanza euclidea da controllare)

--- include/Robot.h
+++ include/Robot.h
@@ -6,6 +6,30 @@
 
 using namespace std;
 
+class Robot{
+
+public:
+    // Posizione del robot
+    double pos_x_, pos_y_;
+    // Velocità del _robot
+    double vel_x_, vel_y_;
+    // Costruttore di default
+    Robot();
+    //costruttore personalizzato
+    Robot(double pos_x_, double pos_y_, double vel_x_, double vel_y_)
+
+    void position_update();
+
+private:
+
+    void separation();    
+    void alignment();
+    void cohesion();
+    void limitSpeed();
+
+    //vettore che conterra tutti i robot creati  
+    static vector<Robot> Robots;
+};
 
 #endif 
 //ROBOT_H
--- libs/Robot.cpp
+++ libs/Robot.cpp
@@ -1,2 +1,37 @@
 #include <Robot.h>
 using namespace std;
+
+'Parametri regolabili'
+
+const double d_sep = 10.0;
+const double voidfactor = 0.01;
+
+'costruttore'
+// inizializzazione statica del vettore 
+vector<Robot>Robot::Robots
+// definizione (inizializzazione) del costruttore 
+Robot::Robot() : pos_x(0.0), pos_y(0.0), vel_x(0.0), vel_y(0.0){}
+
+'creazione delle funzioni'
+
+// Funzione di separazione
+    void separation() {
+        // Accumulare le differenze di posizione 
+        double close_diff_pos_x = 0.0;
+        double close_diff_pos_y = 0.0; 
+
+        // Ciclo su tutti gli altri boid
+        for (const Robot& other_Robot : Robots) {
+            //calcolo la distanza Eucl
+            double distance = std::hypot(Robot.pos_x - other_Robot.pos_x, Robot.pos_y - other_Robot.pos_y);
+            
+            if (distance > 0 && distance < d_sep) {
+                close_diff_pos_x += Robot.pos_x - other_Robot.pos_x;
+                close_diff_pos_y += Robot.pos_y - other_Robot.pos_y;
+            }
+        }
+
+        // Aggiorna la velocità in base alla separazione
+        Robot.vel_x += close_diff_pos_x * avoidfactor;
+        Robot.vel_y += close_diff_pos_y * avoidfactor;
+    }
